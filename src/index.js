ebc36015e6dee258d500b7e9457515efe58becce
fernandoc1 <fernandoc@epidataconsulting.com>
2021-01-03 02:22:01 -0300

    configuracion cors

--- src/index.js
+++ src/index.js
@@ -12,6 +12,16 @@ app.use(express.json());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 //Router
 app.use(require('./router/swagger'))
 app.use(require('./router/authentication'));
